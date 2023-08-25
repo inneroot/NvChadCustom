cf5f1c0771102a36860f4bcc65f4e12c4418e7c5
Gleb Fedotov <innercoding@gmail.com>
2023-08-25 20:10:21 +0300

    scroll bindings

--- mappings.lua
+++ mappings.lua
@@ -4,6 +4,11 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<C-u>"] = { "<C-u>zz", desc = "Page Up" },
+    ["<C-d>"] = { "<C-d>zz", desc = "Page Down" },
+  },
+  v = {
+    ["<leader>p"] = { '"_dP', desc = "paste w/o copy deleted" },
   },
 }
 
